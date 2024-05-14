2d29d9e449e5efc08ccdd4774e63d394c5041f06
SehyeonKang <gedfgrdt0@gmail.com>
2024-05-14 15:27:16 +0900

    Refactor: 로그인 여부, 로그인 멤버 정보 Pinia 적용

--- src/components/group/GroupWrite.vue
+++ src/components/group/GroupWrite.vue
@@ -1,249 +1,239 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import { getMemberProfile } from "@/api/member";
-import { registGroup } from "@/api/group";
+  import { ref, onMounted } from "vue";
+  import { useRouter } from "vue-router";
+  import { registGroup } from "@/api/group";
+  import { useMemberStore } from "@/stores/member";
+  import { storeToRefs } from "pinia";
 
-const router = useRouter();
-const creatorId = ref("");
-const name = ref("");
-const nickname = ref("");
-const location = ref("");
-const maxMemberCount = ref("");
-const startDate = ref("");
-const endDate = ref("");
-const overview = ref("");
-const files = ref([]);
+  const router = useRouter();
 
-const token = ref("");
-// 사용자 정보 가져오기
-const fetchProfile = async () => {
-  token.value = localStorage.getItem("accessToken");
-  if (token) {
-    try {
-      const response = await getMemberProfile(token.value);
+  const memberStore = useMemberStore();
+  const { userInfo } = storeToRefs(memberStore);
 
-      if (response.data) {
-        creatorId.value = response.data.id;
-        nickname.value = response.data.nickname;
-      }
-    } catch (error) {
-      console.error("프로필 정보 조회 실패:", error);
-    }
-  }
-};
+  const creatorId = ref("");
+  const name = ref("");
+  const nickname = ref("");
+  const location = ref("");
+  const maxMemberCount = ref("");
+  const startDate = ref("");
+  const endDate = ref("");
+  const overview = ref("");
+  const files = ref([]);
+  const token = ref("");
 
-const handleFiles = (event) => {
-  files.value = event.target.files;
-}
-
-// 모임 글을 작성하는 함수
-const createGroup = async () => {
-  const groupData = new FormData();
-  const group = {
-    name: name.value,
-    creatorId: creatorId.value,
-    location: location.value,
-    maxMemberCount: maxMemberCount.value,
-    startDate: startDate.value,
-    endDate: endDate.value,
-    overview: overview.value,
+  const handleFiles = (event) => {
+    files.value = event.target.files;
   };
 
-  const groupDto = new Blob([JSON.stringify(group)], {
-    type: 'application/json'
-  })
-  
-  groupData.append('group', groupDto);
+  // 모임 글을 작성하는 함수
+  const createGroup = async () => {
+    const groupData = new FormData();
+    const group = {
+      name: name.value,
+      creatorId: creatorId.value,
+      location: location.value,
+      maxMemberCount: maxMemberCount.value,
+      startDate: startDate.value,
+      endDate: endDate.value,
+      overview: overview.value,
+    };
 
-  for (let i = 0; i < files.value.length; i++) {
-    groupData.append('upfile', files.value[i]);
-  }
-  console.log(groupData);
-  try {
-    const response = await registGroup(token.value, groupData);
-    if (response.status === 201) {
-      router.replace({
-        name: "groupDetail",
-        params: { id: response.data.id },
-      });
+    const groupDto = new Blob([JSON.stringify(group)], {
+      type: "application/json",
+    });
+
+    groupData.append("group", groupDto);
+
+    for (let i = 0; i < files.value.length; i++) {
+      groupData.append("upfile", files.value[i]);
     }
-  } catch (error) {
-    console.error("모임 글 등록 실패:", error);
-  }
-};
+    
+    try {
+      token.value = sessionStorage.getItem("accessToken");
+      const response = await registGroup(token.value, groupData);
+      if (response.status === 201) {
+        router.replace({
+          name: "groupDetail",
+          params: { id: response.data.id },
+        });
+      }
+    } catch (error) {
+      console.error("모임 글 등록 실패:", error);
+    }
+  };
 
-onMounted(() => {
-  fetchProfile();
-});
+  onMounted(() => {
+    creatorId.value = userInfo.value.id;
+    nickname.value = userInfo.value.nickname;
+  });
 </script>
 
 <template>
+  <div>
     <div>
-        <div>
-            <main>
-                <div class="container">
-                    <div class="row mt-5 justify-content-center">
-                        <div class="col-lg-5">
-                            <form>
-                                <div class="form-group">
-                                    <label for="name">모임명</label>
-                                    <input
-                                    type="text"
-                                    class="form-control"
-                                    id="name"
-                                    name="name"
-                                    v-model="name"
-                                    required
-                                    />
-                                </div>
-                                <div class="form-group">
-                                    <label for="creator">모임장</label>
-                                    <input
-                                    type="text"
-                                    class="form-control"
-                                    id="creator"
-                                    name="creator"
-                                    :value="nickname"
-                                    readonly
-                                    />
-                                </div>
-                                <div class="form-group">
-                                    <label for="location">모임 지역</label>
-                                    <input
-                                    type="text"
-                                    class="form-control"
-                                    id="location"
-                                    name="location"
-                                    v-model="location"
-                                    required
-                                    />
-                                </div>
-                                <div class="form-group">
-                                    <label for="maxMemberCount">최대 인원수</label>
-                                    <input
-                                    type="number"
-                                    class="form-control"
-                                    id="maxMemberCount"
-                                    name="maxMemberCount"
-                                    min="1"
-                                    max="100"
-                                    v-model="maxMemberCount"
-                                    required
-                                    />
-                                </div>
-                                <div class="form-group">
-                                    <label class="form-heading">모임 날짜</label>
-                                    <div class="date-field">
-                                        <label for="startDate">시작일</label>
-                                        <input
-                                        type="date"
-                                        class="form-control"
-                                        id="startDate"
-                                        name="startDate"
-                                        v-model="startDate"
-                                        required
-                                        />
-                                    </div>
-                                    <div class="date-field">
-                                        <label for="endDate">종료일</label>
-                                        <input
-                                        type="date"
-                                        class="form-control"
-                                        id="endDate"
-                                        name="endDate"
-                                        v-model="endDate"
-                                        required
-                                        />
-                                    </div>
-                                </div>
-                                
-                                <div class="form-group">
-                                    <label for="overview">상세 설명</label>
-                                    <textarea
-                                    class="form-control"
-                                    id="overview"
-                                    name="overview"
-                                    rows="5"
-                                    v-model="overview"
-                                    required
-                                    ></textarea>
-                                </div>
-                                <input class="file-upload-btn" type="file" multiple @change="handleFiles">
-                                
-                                <div class="text-right">
-                                    <button @click.prevent="createGroup" class="btn btn-primary btn-block write-btn">
-                                    등록
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-
-                        <div class="col-lg-5 image-area border border-secondary">
-                            <div class="row-md-5 side-image-area">
-                                <img src="@/assets/images/groupWriteFormImage.jpg" class="side-image" alt="">
-                            </div>
-                        </div>
-                    </div>
+      <main>
+        <div class="container">
+          <div class="row mt-5 justify-content-center">
+            <div class="col-lg-5">
+              <form>
+                <div class="form-group">
+                  <label for="name">모임명</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="name"
+                    name="name"
+                    v-model="name"
+                    required
+                  />
                 </div>
-            </main>
+                <div class="form-group">
+                  <label for="creator">모임장</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="creator"
+                    name="creator"
+                    :value="nickname"
+                    readonly
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="location">모임 지역</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="location"
+                    name="location"
+                    v-model="location"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="maxMemberCount">최대 인원수</label>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="maxMemberCount"
+                    name="maxMemberCount"
+                    min="1"
+                    max="100"
+                    v-model="maxMemberCount"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label class="form-heading">모임 날짜</label>
+                  <div class="date-field">
+                    <label for="startDate">시작일</label>
+                    <input
+                      type="date"
+                      class="form-control"
+                      id="startDate"
+                      name="startDate"
+                      v-model="startDate"
+                      required
+                    />
+                  </div>
+                  <div class="date-field">
+                    <label for="endDate">종료일</label>
+                    <input
+                      type="date"
+                      class="form-control"
+                      id="endDate"
+                      name="endDate"
+                      v-model="endDate"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="overview">상세 설명</label>
+                  <textarea
+                    class="form-control"
+                    id="overview"
+                    name="overview"
+                    rows="5"
+                    v-model="overview"
+                    required
+                  ></textarea>
+                </div>
+                <input class="file-upload-btn" type="file" multiple @change="handleFiles" />
+
+                <div class="text-right">
+                  <button @click.prevent="createGroup" class="btn btn-primary btn-block write-btn">
+                    등록
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            <div class="col-lg-5 image-area border border-secondary">
+              <div class="row-md-5 side-image-area">
+                <img src="@/assets/images/groupWriteFormImage.jpg" class="side-image" alt="" />
+              </div>
+            </div>
+          </div>
         </div>
-    </div>  
+      </main>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.image-area {
-  margin-right: 30px;
-  border-radius: 15px;
-  box-shadow: 2px 2px 2px 2px rgba(200, 200, 200, 0.8);
-  background-color: rgb(255, 255, 255, 0.6);
-}
+  .image-area {
+    margin-right: 30px;
+    border-radius: 15px;
+    box-shadow: 2px 2px 2px 2px rgba(200, 200, 200, 0.8);
+    background-color: rgb(255, 255, 255, 0.6);
+  }
 
-.side-image-area {
-  width: auto;
-  height: 790px;
-  margin-top: 15px;
-  border-radius: 10px;
-  overflow: hidden;
-}
+  .side-image-area {
+    width: auto;
+    height: 790px;
+    margin-top: 15px;
+    border-radius: 10px;
+    overflow: hidden;
+  }
 
-.side-image {
+  .side-image {
     width: 100%;
     height: auto;
     object-fit: cover;
-}
+  }
 
-.form-heading,
-label {
-  font-size: 16px;
-  font-weight: bold;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  background-color: #f8f9fa;
-  padding: 5px 10px;
-  border-radius: 5px;
-}
+  .form-heading,
+  label {
+    font-size: 16px;
+    font-weight: bold;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    background-color: #f8f9fa;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
 
-.date-field {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-}
+  .date-field {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
 
-.date-field label {
-  margin-right: 10px;
-  white-space: nowrap;
-}
+  .date-field label {
+    margin-right: 10px;
+    white-space: nowrap;
+  }
 
-.form-control {
-  flex-grow: 1; 
-}
+  .form-control {
+    flex-grow: 1;
+  }
 
-.file-upload-btn {
-  margin-top: 10px;
-}
+  .file-upload-btn {
+    margin-top: 10px;
+  }
 
-.write-btn {
-  margin-top: 10px;
-}
+  .write-btn {
+    margin-top: 10px;
+  }
 </style>
--- src/components/group/item/GroupListItem.vue
+++ src/components/group/item/GroupListItem.vue
@@ -1,34 +1,33 @@
 <script setup>
-import { ref, defineProps } from "vue";
-import { useRouter } from "vue-router";
+  import { ref, defineProps } from "vue";
+  import { useRouter } from "vue-router";
 
-const router = useRouter();
+  const router = useRouter();
 
-const props = defineProps({
-  groupItem: Object,
-});
-
-const group = ref("");
-group.value = props.groupItem;
-group.value.startDate = formatVisitedDate(group.value.startDate);
-group.value.endDate = formatVisitedDate(group.value.endDate);
-console.log(props.groupItem);
-
-// 날짜 포맷 변경 함수
-function formatVisitedDate(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${year}/${month.toString().padStart(2, "0")}/${day.toString().padStart(2, "0")}`;
-}
-
-function goGroupDetail() {
-  router.push({
-    name: "groupDetail",
-    params: { id: group.value.id },
+  const props = defineProps({
+    groupItem: Object,
   });
-}
+
+  const group = ref("");
+  group.value = props.groupItem;
+  group.value.startDate = formatVisitedDate(group.value.startDate);
+  group.value.endDate = formatVisitedDate(group.value.endDate);
+
+  // 날짜 포맷 변경 함수
+  function formatVisitedDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}/${month.toString().padStart(2, "0")}/${day.toString().padStart(2, "0")}`;
+  }
+
+  function goGroupDetail() {
+    router.push({
+      name: "groupDetail",
+      params: { id: group.value.id },
+    });
+  }
 </script>
 
 <template>
@@ -66,78 +65,78 @@ function goGroupDetail() {
 </template>
 
 <style scoped>
-.card {
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
-  margin: 10px;
-  overflow: hidden; /* 카드 범위 밖 내용 숨기기 */
-  cursor: pointer;
-}
-.card:hover .group-location {
-  color: white; /* 호버 시 글자 색 변경 */
-  background-color: #00a1fc; /* 호버 시 배경 색 변경 */
-}
-.card:hover .group-image {
-  transform: scale(1.15); /* 이미지 확대 */
-  transition: transform 0.3s ease; /* 이미지 확대 트랜지션 효과 추가 */
-}
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+    margin: 10px;
+    overflow: hidden; /* 카드 범위 밖 내용 숨기기 */
+    cursor: pointer;
+  }
+  .card:hover .group-location {
+    color: white; /* 호버 시 글자 색 변경 */
+    background-color: #00a1fc; /* 호버 시 배경 색 변경 */
+  }
+  .card:hover .group-image {
+    transform: scale(1.15); /* 이미지 확대 */
+    transition: transform 0.3s ease; /* 이미지 확대 트랜지션 효과 추가 */
+  }
 
-.card-header {
-  height: 50px;
-  font-size: 11px;
-  border-bottom: none; /* 하단 테두리 제거 */
-}
-.card-title {
-  font-size: 17px;
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 1; /* 1줄로 제한 */
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: normal;
-  height: 1.5em; /* 높이 조절을 통해 텍스트 라인의 개수와 높이를 일치시킴 */
-}
-.custom-vr {
-  border-left: 1px solid lightgray;
-  height: 30px;
-  align-self: center;
-}
-.writer-nickname {
-  font-size: 13px;
-}
-.travel-date-icon {
-  color: gray;
-}
-.travel-date-label {
-  margin-left: 3px;
-  color: gray;
-}
-.travel-date {
-  color: #00a1fc;
-}
-.group-image-area {
-  height: 160px;
-  overflow: hidden;
-  border-radius: 10px;
-}
-.group-image {
-  border-radius: 10px;
-  transition: transform 0.3s ease; /* 이미지 트랜지션 효과 추가 */
-}
-.group-location {
-  top: 10px;
-  left: 10px;
-  height: 25px;
-  padding-top: 2px;
-  padding-left: 10px;
-  padding-right: 10px;
-  color: #00a1fc;
-  background-color: white;
-  border-radius: 10px;
-  font-size: 14px;
-}
-.fa-calendar {
-  margin-top: 2px;
-}
+  .card-header {
+    height: 50px;
+    font-size: 11px;
+    border-bottom: none; /* 하단 테두리 제거 */
+  }
+  .card-title {
+    font-size: 17px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1; /* 1줄로 제한 */
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    height: 1.5em; /* 높이 조절을 통해 텍스트 라인의 개수와 높이를 일치시킴 */
+  }
+  .custom-vr {
+    border-left: 1px solid lightgray;
+    height: 30px;
+    align-self: center;
+  }
+  .writer-nickname {
+    font-size: 13px;
+  }
+  .travel-date-icon {
+    color: gray;
+  }
+  .travel-date-label {
+    margin-left: 3px;
+    color: gray;
+  }
+  .travel-date {
+    color: #00a1fc;
+  }
+  .group-image-area {
+    height: 160px;
+    overflow: hidden;
+    border-radius: 10px;
+  }
+  .group-image {
+    border-radius: 10px;
+    transition: transform 0.3s ease; /* 이미지 트랜지션 효과 추가 */
+  }
+  .group-location {
+    top: 10px;
+    left: 10px;
+    height: 25px;
+    padding-top: 2px;
+    padding-left: 10px;
+    padding-right: 10px;
+    color: #00a1fc;
+    background-color: white;
+    border-radius: 10px;
+    font-size: 14px;
+  }
+  .fa-calendar {
+    margin-top: 2px;
+  }
 </style>
